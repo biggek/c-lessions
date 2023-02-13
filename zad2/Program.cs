9fcf4b1a20f53b732192f366364c6ce7df54fb5b
Sergey <lolito0700@mail.ru>
2023-02-13 11:19:53 +0300

    zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿string aString;
+string bString;
+int b = 0;
+int a = 0;
+Console.Write("Введите первое число");
+aString = Console.ReadLine();
+a = Convert.ToInt32(aString);
+Console.Write("Введите второе число");
+bString = Console.ReadLine();
+b = Convert.ToInt32(bString);
+if (a>b)
+{
+    Console.Write($"Наибольшее число {a}, Меньшее число {b}");
+}
+else if (b>a)
+{
+    Console.Write($"Наибольшее число {b}, Меньшее число {a}");
+}	
