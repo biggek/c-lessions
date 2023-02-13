5e5003821beb904ae26fd40d00722baa8e7c37a7
Sergey <lolito0700@mail.ru>
2023-02-13 11:22:07 +0300

    zad6

---
+++ zad6/Program.cs
@@ -0,0 +1,14 @@
+﻿int i;
+Console.Write("Введите число : ");
+i = int.Parse(Console.ReadLine());
+ if (i % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+    Console.Read(); 
+}
+    else
+  {
+    Console.Write("Введенное число нечетное");
+  }
+ 
+            
